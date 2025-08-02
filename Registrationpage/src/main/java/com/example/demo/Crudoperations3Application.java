5ecd87524aa17954c2b4a897ac9c61f9aa8d3023
Prabhaseslavath <eslavathprabhas333@gmail.com>
2025-08-02 15:25:32 +0530

    Registration page

---
+++ Registrationpage/src/main/java/com/example/demo/Crudoperations3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudoperations3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crudoperations3Application.class, args);
+	}
+
+}
