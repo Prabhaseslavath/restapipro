890dff1915cf2d8dc69b5265bfd3d91e75f0956f
Prabhaseslavath <eslavathprabhas333@gmail.com>
2025-08-02 14:37:31 +0530

    orderpage

---
+++ crudoperations1/src/main/java/com/example/demo/Crudoperations1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudoperations1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crudoperations1Application.class, args);
+	}
+
+}
